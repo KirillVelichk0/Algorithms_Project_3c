05d4ceab705a9f7a3d73912afe339e57bc90b85a
Kirill <cirill@live.ru>
2023-04-26 19:18:47 +0300

    Add references to comparators (fix my forigveness)

--- src/StandartSort/StandartSort.hpp
+++ src/StandartSort/StandartSort.hpp
@@ -3,7 +3,7 @@ class StandartSorter{
 public:
     template <class It, class Comparator>
     void sort(It begin, It end, Comparator comparator){
-        auto comparatorForStdSort = [&comparator](auto lhs, auto rhs){
+        auto comparatorForStdSort = [&comparator](const auto& lhs, const auto& rhs){
             return comparator(lhs, rhs) == 1;
         };
         std::sort(begin, end, comparatorForStdSort);
