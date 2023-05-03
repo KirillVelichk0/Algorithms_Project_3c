b4bca4c897e1f7bd4b9636efc747666e5dd2d217
Kirill <cirill@live.ru>
2023-05-04 01:45:01 +0300

    Add a recursive radix sort

---
+++ src/RadixSort/Radix.hpp
@@ -0,0 +1,75 @@
+/*Поразрядная сортировка сильно отличается тем,
+что для нее не подходит стандартный интерфейс компаратором
+Помимо сравнения(или, вместо), поразрядная сортировка должна иметь интерфейс 
+преобразования элемента в беззнаковое число*/
+//Для лучшего результата, буду работать с байтами, что позволит избежать дорогих операций деления
+//Однако, на разных платформах, порядок байт может отличаться. Его надо узнать
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+bool IsBigEndian(){
+    constexpr std::int32_t a = 0xA1B2C3D4;
+    unsigned char *p = (unsigned char*)&a;
+    return *p == 0xA1;
+}
+class RadixSorterRecursive{
+private:
+    template <class BiecIt, bool isBigEndian, std::int16_t limRadix = 8>
+    void rec_sort(BiecIt begin, BiecIt end, std::int16_t curRadix){
+        if(curRadix < limRadix){
+            std::int16_t curPos;
+            if constexpr (isBigEndian){
+                curPos = limRadix - 1 - curRadix;
+            }
+            else{
+                curPos = curRadix;
+            }
+            using InternalType = std::decay_t<decltype(*begin)>;
+            std::vector<std::vector<InternalType>> radixes(256);
+            auto curIt = begin;
+            while(curIt != end){
+                auto& val = *curIt;
+                unsigned char* bytes_val = (unsigned char*)(&(val.first));
+                unsigned char curRadixByte = bytes_val[curPos];
+                radixes[curRadixByte].push_back(val);
+                curIt = std::next(curIt, 1);
+            }
+
+            std::for_each(radixes.cbegin(), radixes.cend(), [curIt = begin](const auto& oneRadix) mutable{
+                std::for_each(oneRadix.cbegin(), oneRadix.cend(), [&curIt](const auto& val){
+                    *curIt = val;
+                    curIt = std::next(curIt, 1);
+                });
+            });
+            this->rec_sort<BiecIt, isBigEndian>(begin, end, curRadix + 1);
+        }
+    }
+public:
+    //По задумке, Mapper обязан возвращать unsigned int64
+    template<class It, class Mapper>
+    void sort(It begin, It end, Mapper& mapper){
+        using InternalType = std::decay_t<decltype(*begin)>;
+        std::vector<std::pair<std::uint64_t, InternalType>> biections;
+        {
+            auto curIt = begin;
+            while(curIt != end){
+                auto val = *curIt;
+                biections.push_back(std::make_pair(mapper(val), val));
+                curIt = std::next(curIt, 1);
+            }
+        }
+        bool isBigEndian = IsBigEndian();
+        using ItType = decltype(biections.begin());
+        if(isBigEndian){
+            this->rec_sort<ItType, true>(biections.begin(), biections.end(), 0);
+        }
+        else{
+            this->rec_sort<ItType, false>(biections.begin(), biections.end(), 0);
+        }
+        auto curIt = begin;
+        for(auto& val: biections){
+            *curIt = val.second;
+            curIt = std::next(curIt, 1);
+        }
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "RadixSort/Radix.hpp"
 #include "Simple/Simple.hpp"
 #include "Shell/Shell.hpp"
 #include "FastSort/FastSort.hpp"
@@ -7,8 +8,8 @@
 #include "StandartSort/StandartSort.hpp"
 #include "MergeSort/MergeSort.hpp"
 int main(int, char**) {
-    std::vector<int> cont = {-11, 1, 10, 7, 15, 100, 10000, -1, 228};
-    MergeSorter sorter;
+    std::vector<std::int32_t> cont = {-11, 1, 10, 7, 15, 100, 10000, -1, 228};
+    RadixSorterRecursive sorter;
     auto comparator = [](auto& left, auto& right) -> int{
         if( right > left){
             return 1;
@@ -20,7 +21,25 @@ int main(int, char**) {
             return -1;
         }
     };
-    sorter.sort(cont.begin(), cont.end(), comparator);
+    auto mapper = [](std::int32_t val){
+        std::uint64_t image;
+        if(val < 0){
+            val -= INT32_MIN;
+            image = val;
+        }
+        else{
+            image = val;
+            image -= INT32_MIN;
+        }
+        
+        return image;
+    };
+    sorter.sort(cont.begin(), cont.end(), mapper);
+    /*std::vector<std::uint64_t> cont = {3, 7, 254, 100, 107};
+    auto mapper = [](std::uint64_t val){
+        return val;
+    };
+    sorter.sort(cont.begin(), cont.end(), mapper);*/
     for(auto val: cont){
         std::cout << val << ' ';
     }
