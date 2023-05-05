0e39dd626a7aa5b56abb947d0d3ac1e3def77341
Kirill <cirill@live.ru>
2023-05-05 18:49:29 +0300

    Add gibrid sort

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,54 @@
 #include "StandartSort/StandartSort.hpp"
 #include "MergeSort/MergeSort.hpp"
 #include "MasterSort/MasterSort.hpp"
+#include <cmath>
+
+class HybrydSorter{
+private:
+    template <class It, class Comparator>
+    auto getPartition(It begin, It end, Comparator comparator)
+    {
+        auto pivot = *begin;
+        auto upIt = begin;
+        auto downIt = std::next(end, -1);
+        while(true){
+            while(upIt != end && comparator(*upIt, pivot) == 1){
+                upIt++;
+            }
+            while(comparator(*downIt, pivot) == -1){
+                downIt--;
+            }
+            if(std::distance(upIt, downIt) <= 0){
+                return std::next(downIt, 1);
+            }
+            std::swap(*upIt, *downIt);
+            upIt++; downIt--;
+        }
+    }
+    template <class It, class Comparator>
+    void rec_sort(It begin, It end, Comparator comparator, std::int16_t curPrice){
+        if(curPrice-- == 0){
+            HeapSorter sorter;
+            sorter.sort(begin, end, comparator);
+            return;
+        }
+        auto size = std::distance(begin, end);
+        if (size < 2)
+        {
+            return;
+        }
+        auto middlePos = this->partitioner.getPartition(begin, end, comparator);
+        this->rec_sort(begin, middlePos, comparator, curPrice);
+        this->rec_sort(middlePos, end, comparator, curPrice);
+    }
+public:
+    template <class It, class Comparator>
+    void sort(It begin, It end, Comparator comparator){
+        auto size = std::distance(begin, end);
+        this->rec_sort(begin, end, comparator, int(std::log2(size)) * 2);
+    }
+};
+
 int main(int, char**) {
     std::vector<std::int32_t> cont = {-100, 13, 1, 10000, -23412, 1234, 312, 3123, 13, 1, 1, -322, 228, 15, 2, 3, 10, -19, 22, 1000};
     FastSorter sorter;
